17d6d11f2e9865a5990eaa2f1fc3315829eae3b9
Rajeev Ranjan <Rajeev.Ranjan@mrisoftware.com>
2023-01-06 16:37:40 +0530

    DAO and exchange

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -65,6 +65,30 @@ async function main() {
     "Crypto Devs Token Contract Address:",
     deployedCryptoDevsTokenContract.address
   );
+
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    cryptoDevsContract.address,
+    {
+      // This assumes your account has at least 1 ETH in it's account
+      // Change this value as you want
+      value: ethers.utils.parseEther("1"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
